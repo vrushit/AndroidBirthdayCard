ee3980c43d35c9822631ac712117c08ac870fe3f
vrushit <34651962+vrushit@users.noreply.github.com>
2018-07-20 21:32:50 +0530

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,13 @@
+//MainActivity.kt this is a Kotlin file file you can easily convert it into java file in ANdroid Studio 
+package com.example.hp.happybirthday
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
